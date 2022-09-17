cb30fc83bda1d8e290cce3e190e500e16edb8fa1
Ewan MARTIN <wanou.martin@gmail.com>
2022-09-17 18:59:55 +0200

    Terminé la section skills html + css + animation des cercles. Reste à régler le déclanchement de l'animation à la vue de l'utilisateur.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,9 @@
+// LOCOMOTIVE SCROLL
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container"),
     smooth: true,
     tablet: {smooth: true},
     smartphone: {smooth: true},
 });
+
