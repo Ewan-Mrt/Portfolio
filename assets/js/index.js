c081654160981249140fb091941e8a182475cc5a
Ewan MARTIN <wanou.martin@gmail.com>
2022-09-18 15:27:18 +0200

    Ajout d'effet d'apparition sur les section.

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,20 @@ const scroll = new LocomotiveScroll({
     smartphone: {smooth: true},
 });
 
+
+// SCROLL OBSERVER
+
+let observer = new IntersectionObserver(observables => {
+    for(let observable of observables){
+        if(observable.intersectionRatio > 0.5)
+            observable.target.classList.remove("hidden");
+    }
+}, {
+   threshold: 0.5 
+});
+
+const sections = document.querySelectorAll("section")
+for(let section of sections){
+    section.classList.add("hidden");
+    observer.observe(section);
+}
